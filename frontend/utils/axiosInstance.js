aa7be3be41cbe323b5b4d585595cca42a914e209
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-08-29 23:43:05 +0530

    Delete frontend directory

--- frontend/utils/axiosInstance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-const axiosInstance = axios.create({
-    baseURL: 'https://aa-rho-rust.vercel.app/'
-});
-
-// Export the axios instance for making HTTP requests
-export default axiosInstance;
